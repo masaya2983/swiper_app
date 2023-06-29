46a8a7ade662de3d0d463ad19bfb8fbca5b67855
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-06-29 10:16:18 +0000

    il

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,14 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-const webpack = require('webpack')
-enviroment.plugins.prepend(
-  'provide',
-  new webpack.Provideplugin({
-    $: 'jquery/src/jquery',
-    jquery: 'jquery/src/jquery',
-    Swiper: 'swiper/swiper-bundle' 
-  })
-  )
-  
-  
 module.exports = environment
